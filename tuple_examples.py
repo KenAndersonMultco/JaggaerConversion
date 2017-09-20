b2e7762ac7d1402ad9a3ad9fed4221c2977cb5ac
KenAndersonMultco <32107386+KenAndersonMultco@users.noreply.github.com>
2017-09-20 14:22:36 -0700

    Adding test script
    Adding a test script to test out the process of making and integrating changes.

---
+++ tuple_examples.py
@@ -0,0 +1,33 @@
+#tuples
+
+#work like lists, but can't be changed in place and usually written as series
+#of items in parentheses, not brackets
+
+#tuples are:
+# 1) ordered collections of arbitrary objects
+# 2) accessed by offset
+# 3) of the category "immutable sequence"
+# 4) fixed-length, heterogeneous, and arbitrarily nestable (e.g. they can
+#    hold other compound objects such as lists, dicts, other tuples
+# 5) arrays of object references
+
+# empty tuple
+t = ()
+
+# one item tuple
+print 'one-item tuple'
+t = (0,)
+print t[0]
+
+# four-item tuple
+
+print 'print 3rd item of a 4-item tuple'
+t = (0, 'Nl', 1.2,3)
+print t[2]
+
+#nested tuple
+
+t= ('abc',('def','ghi'))
+print 'print 2nd item of 2nd item in nested tuple'
+print t[1][1]
+
