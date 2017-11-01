a5410e9021f4b6e41af91571f7c13bee4a70ae9c
ANDERSON Ken <anderskr@co.multnomah.or.us>
2017-10-31 18:27:16 -0700

    Simplified logic based on fact that all subfolders underneath the main contract folder will be removed as the first step.

--- ContractFileConversion_BuildLookup.py
+++ ContractFileConversion_BuildLookup.py
@@ -4,25 +4,9 @@ import csv
 
 #prompt user to select top level folder for documents
 
-#adding a comment to test working with Github
-
-#just for fun, lets see what happens if I open the file from
-#the github folder in Idle and make a change.
-
-##for fname in os.listdir('\\nas3\CW-IT\Projects\RM_Docs\BUS2000113'):
-##    print fname
-
-
-##dir = 'R:\BUS2000113'
-##for fname in os.listdir(dir):
-##    fpath = os.path.join(dir,fname)
-##    print fname,os.path.isfile(fpath), os.path.isdir(fpath)
-
 
 fout = open('h:/JaggaerDC/ContractFileData.csv','wb')
 mywriter = csv.writer(fout,delimiter = ',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
-
-
 
 #Build lookup file with contract name and associated documents
 
@@ -30,42 +14,41 @@ dir = 'R:\BUS2000113'
 contractfolders = sorted(os.listdir(dir))
 
 for f in contractfolders:
-    cdir = os.path.join(dir,f)
     ContractNumber = f[0:10]
-    print 'Contract Number is',ContractNumber
-    for root, dirs, files in os.walk(cdir):
-        if len(dirs) == 0 and len(files) > 0:  #this is a tuple that has files
-            #filenames = ''
-            maxfilename = ''
-            maxfilesize = 0
-            namelist = []
-            lineout = []
-            for fname in files:
-                namelist.append(fname)
-                filepath = os.path.join(root,fname)
-                size = os.path.getsize(filepath)
-                if size > maxfilesize:
-                    maxfilename = fname
-                    maxfilesize = size
-                #displayname = fname2[17:]
-                #print displayname, size
-            #print 'largest file is',maxfilename
+    lineout = []
+    namelist = []
+    maxfilename = ''
+    maxfilesize = 0
+    for fname in os.listdir(os.path.join(dir,f)):       
+        namelist.append(fname)
+        filepath = os.path.join(dir,f,fname)
+        size = os.path.getsize(filepath)
+        if size > maxfilesize:
+            maxfilename = fname
+            maxfilesize = size
+        #displayname = fname2[17:]
+        #print displayname, size
+        #print 'largest file is',maxfilename
 
-            #build lookup file row
-            #
-            lineout.append(ContractNumber)
-            #write main document
-            lineout.append(maxfilename[17:]+'!'+maxfilename)
-            #get additional attachments
-            attachments = ''
-            for x in namelist:
-                if x != maxfilename:
-                    filestuff = x[17:] + '!' + x
-                    if attachments == '':
-                        attachments = attachments + filestuff
-                    else:
-                        attachments = attachments + '|' +  filestuff
-            lineout.append(attachments)
-            mywriter.writerow(lineout)
+    #build lookup file row
+    lineout.append(ContractNumber)
+    #write main document
+    lineout.append(maxfilename[17:]+'!'+maxfilename)
+    #get additional attachments
+    attachments = ''
+    for x in namelist:
+        if x != maxfilename:
+            filestuff = x[17:] + '!' + x
+            if attachments == '':
+                attachments = attachments + filestuff
+            else:
+                attachments = attachments + '|' +  filestuff
+    lineout.append(attachments)
+    mywriter.writerow(lineout)
 
 fout.close()
+
+
+
+
+
